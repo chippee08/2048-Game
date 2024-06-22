1858d9d2b0b4286e0b17edabe058cc3ded91445a
chippee08 <paolobanag08@gmail.com>
2024-06-22 16:15:10 +0800

    Update logic.js

--- logic.js
+++ logic.js
@@ -86,3 +86,41 @@ function handleSlide(e){
 }
 
 document.addEventListener("keydown", handleSlide);
+
+
+function filterZero(){
+    return row.filter(num => num != 0);
+}
+
+function slide(){
+    row = filterZero(row);
+
+    for(let i = 0; i < row.length - 1; i++){
+        if(row[i] == row[i+1]){
+            row[i] *= 2;
+            row[i+1] = 0;
+        }
+    }
+    row = filterZero(row);
+
+    // Add zeroes on the back after merging
+	while(row.length < columns){
+		row.push(0);
+	}
+
+	return row;
+}
+
+function slideLeft(){
+    for(let r = 0; r < rows; r++){
+        let row = board[r];
+        row = slide(row);
+        board[r] = row;
+        
+        for(let c = 0; c < columns; c++){
+            let tile = document.getElementById(r.toString() + "-" + c.toString());
+            let num = board[r][c];
+            updateTile(tile, num);
+        }
+    }
+}
